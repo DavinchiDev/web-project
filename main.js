ea001554facbec8d27973bd07fe44775d9ea18eb
DavinchiDev <80538770+DavinchiDev@users.noreply.github.com>
2021-03-24 02:24:53 -0500

    some changes
    implementacion de javascript (incompleta), se comprimen imagenes para aumentar rendimiento general, algunos cambios en indez y styles

---
+++ main.js
@@ -0,0 +1,9 @@
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".nav-menu").classList.toggle("show");
+});
+ 
+ ScrollReveal().reveal('.showcase');
+ ScrollReveal().reveal('.info-cards', { delay: 500 });
+ ScrollReveal().reveal('.banner-card-1', { delay: 500 });
+ ScrollReveal().reveal('.banner-card-2', { delay: 500 });
+ ScrollReveal().reveal('.social', { delay: 500 });
